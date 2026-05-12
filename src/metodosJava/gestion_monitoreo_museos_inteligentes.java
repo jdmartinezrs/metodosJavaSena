2f1b1d544babd85180fb356a91abfe698adbfda5
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-05-12 01:37:27 +0000

    Se inicia identificando la mayría de métodos posibles que serán utilizados

--- src/metodosJava/gestion_monitoreo_museos_inteligentes.java
+++ src/metodosJava/gestion_monitoreo_museos_inteligentes.java
@@ -4,10 +4,16 @@ public class gestion_monitoreo_museos_inteligentes {
   
     // El sistema debe registrar información sobre piezas de arte, temperatura ambiental, 
     // niveles de humedad, iluminación y movimientos de personas dentro de las diferentes salas del museo. 
+
     // La complejidad del problema surge porque muchas obras poseen un valor histórico incalculable
     // y requieren condiciones ambientales estrictamente controladas para evitar deterioro. 
+
     // El programa debe analizar continuamente datos de sensores ambientales para detectar cambios peligrosos de temperatura, 
-    // humedad o exposición a la luz que puedan afectar pinturas, esculturas o documentos antiguos. Además, 
+    // humedad o exposición 
+
+    // a la luz que puedan afectar pinturas, esculturas o documentos antiguos.
+    //  Además, 
+
     // el sistema debe controlar accesos de visitantes, monitorear cámaras de seguridad y detectar comportamientos 
     // sospechosos o intentos de robo mediante inteligencia de video y sensores de movimiento.
     //  Cuando se detecte una anomalía, el sistema debe activar alarmas automáticas y notificar al personal de seguridad. 
@@ -17,6 +23,40 @@ public class gestion_monitoreo_museos_inteligentes {
     // El diseño debe ser modular y escalable para permitir futuras integraciones con realidad aumentada,
     // inteligencia artificial y sistemas interactivos avanzados para mejorar la experiencia cultural de los visitantes. 
 
-    
+    public String resgitrarObrasDeArte (String [] obrasDeArte){
+    //registrar si posee valor historico
+    //Registrar Tipo de obra de arte
 
+    }
+
+    public double registrarTemperaturaDelAmbiente (double temperatura){
+    double temperaturaAmbienteIdeal = 0;
+
+    }
+
+    public double revisionTemperatura (double revisionTemperatura ){
+    //detectar cambios peligrosos de temperatura
+
+    }
+    
+    public double registrarNivelesDeHumedad ( double humedad ){
+
+    }
+
+      public double revisionHumedad (double revisionTemperatura ){
+    //detectar cambios peligrosos de Humedad
+
+    }
+
+    public double registrarNivelesDeIluminacion (double iluminacion){
+    }
+
+    
+    public double revisionIluminacion (double iluminacion){
+    }
+
+    public static void main(String[] args){
+
+        
+    }
 }
