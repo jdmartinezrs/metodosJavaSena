13625504e50c4789b08ed336fa153a252a06799c
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-05-14 21:16:16 +0000

    Se da inicio a la actividad 3.3

---
+++ src/actividadtres/Monitor_purificacion_aire_en_tunel_sena.java
@@ -0,0 +1,24 @@
+package actividadtres;
+
+public class Monitor_purificacion_aire_en_tunel_sena {
+
+    //Usted ha sido contratado para desarrollar el software de monitoreo ambiental de un túnel 
+    // vial de largo alcance.
+    // 
+    // El sistema recibe los niveles de monóxido de carbono (CO) y 
+    // la densidad de visibilidad (neblina o humo) reportada por los sensores láser.
+    //  A partir de esta información, el programa debe calcular el "Índice de Calidad del Aire Interior".
+
+    //  El Ministerio de Transporte ha fijado un "Límite de Exposición Humana" 
+    // que es una constante legal para todos los túneles del país, 
+    // permitiendo que el cálculo de riesgo se realice de forma global. 
+
+    // Tras obtener el índice de calidad, el sistema debe gestionar los extractores:
+    //  si el aire es óptimo, se mantienen al mínimo; si es deficiente, 
+    // se activan a plena potencia y se cierran los peajes de entrada.
+    
+    //  El sistema debe garantizar que si el nivel de CO llega a un punto de toxicidad extrema, 
+    // se disparen las alarmas de evacuación y se genere un historial que guarde la densidad de visibilidad 
+    // y el número de vehículos que se encuentran dentro del túnel en ese momento. 
+
+}
